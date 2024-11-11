e68d81a9844a82b51bdadefce55ddcc28c847caf
yordanovsstoyan <stoyan.yordanov@bakdata.com>
2024-11-11 16:34:01 +0200

    remove some vars

--- autoscaler-manifest.tf
+++ autoscaler-manifest.tf
@@ -1,3 +1,15 @@
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
+}
+
 resource "kubectl_manifest" "service_account" {
   yaml_body = <<-EOF
 apiVersion: v1
