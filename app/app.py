f9ba78e47ca08e76292ad09b17abed8b6c5bac1a
yordanovsstoyan <stoyan.yordanov@bakdata.com>
2024-11-11 21:35:56 +0200

    tfversion

--- app/app.py
+++ app/app.py
@@ -1,32 +1,23 @@
 import os
 import pymysql
 
-# Retrieve database connection details from environment variables
-host = os.getenv('MYSQL_HOST', 'localhost')  # Default to 'localhost' if not set
-user = os.getenv('MYSQL_USER', 'root')       # Default to 'root' if not set
-password = os.getenv('MYSQL_PASSWORD', '')   # Default to empty string if not set
-database = os.getenv('MYSQL_DB', 'test')   # Default to 'test' if not set
+host = os.getenv('DB_HOST', 'localhost')
+user = os.getenv('DB_USER', 'admin')
+password = os.getenv('DB_PASSWORD', 'password')
 
 connection = None
 try:
-    # Establish a connection to the database
+    # Connect to DB
     connection = pymysql.connect(
         host=host,
         user=user,
-        password=password,
-        database=database
+        password=password
     )
-
-    # If connection is successful
-    print(f"Connection to MySQL database '{database}' successful!")
-
-    # You can perform your queries here if needed
+    print(f"Connection to MySQL database successful! Hello World")
 
 except pymysql.MySQLError as e:
-    # Handle any errors during connection
     print(f"Error connecting to MySQL database: {e}")
 finally:
-    # Close the connection if it's open
     if connection:
         connection.close()
         print("Connection closed.")
