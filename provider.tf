a3b7c3ad6e1dc2c65e03cfce310fded7e07daa46
yordanovsstoyan <stoyan.yordanov@bakdata.com>
2024-11-11 16:30:22 +0200

    remove some vars

--- provider.tf
+++ provider.tf
@@ -23,7 +23,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  load_config_file       = false
+  token                  = data.aws_eks_cluster_auth.default.token
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
