c783f99ddc46ce604d8645c277393a83db9ee07d
yordanovsstoyan <stoyan.yordanov@bakdata.com>
2024-11-11 16:25:02 +0200

    remove some vars

--- provider.tf
+++ provider.tf
@@ -5,8 +5,9 @@ terraform {
       version = ">4.66.1"
     }
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
+      source           = "gavinbunney/kubectl"
+      version          = ">= 1.14.0"
+      load_config_file = false
     }
     helm = {
       source  = "hashicorp/helm"
@@ -23,7 +24,7 @@ provider "aws" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.default.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-
+  
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
