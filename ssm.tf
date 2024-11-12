8eff95f463b5f0f5550f8ff83d3f51a1bdcef556
yordanovsstoyan <stoyan.yordanov@bakdata.com>
2024-11-12 16:54:19 +0200

    test ssm

--- ssm.tf
+++ ssm.tf
@@ -10,10 +10,28 @@ resource "aws_ssm_parameter" "db_password" {
   value = random_password.db.result
 }
 
+resource "aws_ssm_parameter" "db_user" {
+  name  = "db_user"
+  type  = "String"
+  value = aws_db_instance.app_db.username
+}
+resource "aws_ssm_parameter" "db_address" {
+  name  = "db_address"
+  type  = "String"
+  value = aws_db_instance.app_db.address
+}
+
+
 resource "aws_ssm_parameter" "ecr_repo_name" {
   name  = "ecr_repo_name"
   type  = "String"
   value = aws_ecr_repository.ecr.name
+}
+
+resource "aws_ssm_parameter" "ecr_registry_url" {
+  name  = "ecr_registry_url"
+  type  = "String"
+  value = aws_ecr_repository.ecr.repository_url
 }
 
 resource "aws_ssm_parameter" "cluster_name" {
